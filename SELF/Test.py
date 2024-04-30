cb79409a27153108253b2e908ce2315d696c2480
RohiniWadje <168447254+RohiniWadje@users.noreply.github.com>
2024-04-30 10:41:07 +0000

    Commit

---
+++ SELF/Test.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title(":rainbow[Hello World]")
+st.header('welcome to streamlit')
+st.markdown("*Streamlit* is **really** ***cool***.")
+st.write("Good Morning")
+st.text_input("Type your question")
+st.write('Hello, **World!** :sunglasses:')
+
+'Rohini Wadje'
+
+st.button(":yellow[Stream data]")
